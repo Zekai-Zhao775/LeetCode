e8df8ddbcbec4006a77e6ef51b7f41cab22c8993
Zekai Zhao <zzhao775@uwo.ca>
2024-12-26 11:06:44 -0500

    Create 242. Valid Anagram.py

---
+++ Python/242. Valid Anagram.py
@@ -0,0 +1,57 @@
+# O(n * 2)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        len_s = len(s)
+        len_t = len(t)
+
+        if len_s != len_t:
+            return False
+
+        i = 0
+
+        while i < len_s:
+            if(s.count(s[i]) != t.count(s[i])):
+                return False
+            else:
+                i += 1
+
+        return True
+
+# O(n)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        len_s = len(s)
+        len_t = len(t)
+
+        if len_s != len_t:
+            return False
+
+        frequency_s = {}
+        frequency_t = {}
+
+        i = 0
+
+        # while i < len_s:
+        #     frequency_s[s[i]] = frequency_s.get(s[i], 0) + 1
+        #     frequency_t[t[i]] = frequency_t.get(t[i], 0) + 1
+        #     i += 1
+
+        for char in s:
+            frequency_s[char] = frequency_s.get(char, 0) + 1
+        for char in t:
+            frequency_t[char] = frequency_t.get(char, 0) + 1
+
+        if frequency_s == frequency_t:
+            return True
+        else:
+            return False
