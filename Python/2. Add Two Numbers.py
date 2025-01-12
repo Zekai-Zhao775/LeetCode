b026757dae1a321637a4344cbcf9e15d8d0b756b
Zekai Zhao <zzhao775@uwo.ca>
2025-01-12 17:30:15 -0500

    Create 2. Add Two Numbers.py

---
+++ Python/2. Add Two Numbers.py
@@ -0,0 +1,68 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# ugly
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        current_l1 = l1
+        current_l2 = l2
+        last_l1_temp = None
+        last_l2_temp = None
+
+        while current_l1 and current_l2:
+            sum = current_l1.val + current_l2.val + carry
+            if sum < 10:
+                current_l1.val = sum
+                carry = 0
+            elif sum >= 10:
+                current_l1.val = sum - 10
+                carry = 1
+            last_l1_temp = current_l1
+            current_l1 = current_l1.next
+            last_l2_temp = current_l2
+            current_l2 = current_l2.next
+
+        return_flag = 1
+
+        if current_l2:
+            last_l1_temp.next = current_l2
+            return_flag = 2
+
+        while current_l1:
+            sum = current_l1.val + carry
+            if sum < 10:
+                current_l1.val = sum
+                carry = 0
+                break
+            elif sum >= 10:
+                current_l1.val = sum - 10
+                carry = 1
+            last_l1_temp = current_l1
+            current_l1 = current_l1.next
+
+        while current_l2:
+            sum = current_l2.val + carry
+            if sum < 10:
+                current_l2.val = sum
+                carry = 0
+                break
+            elif sum >= 10:
+                current_l2.val = sum - 10
+                carry = 1
+            last_l2_temp = current_l2
+            current_l2 = current_l2.next
+
+        if return_flag == 1:
+            if carry == 1:
+                last_l1_temp.next = ListNode(1)
+            return l1
+        else:
+            if carry == 1:
+                last_l2_temp.next = ListNode(1)
+            return l1
