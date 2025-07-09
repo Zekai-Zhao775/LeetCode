26b7df0acb1f14ead75daf0fd961e10e4b384195
Zekai Zhao <zzhao775@uwo.ca>
2025-07-09 16:34:55 -0400

    Create 136. Single Number.py

---
+++ LeetCode/136. Single Number.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # need O(n) time and O(1) space
+        # dictionary O(n) time O(n) space, skip
+
+        # use list itself to tract single
+        # pop it when see it again, nums[i] in nums[:i]?
+        # O(n*n) time, O(1)
+
+        # sort it first then scan, O(logn) time and O(1) space
+
+        # use xor
+        # a xor a = 0, a ^ a = 0
+        # a xor 0 = a, a ^ 0 = a
+        # 0 xor 0 = 0, 0 ^ 0 = 0
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
