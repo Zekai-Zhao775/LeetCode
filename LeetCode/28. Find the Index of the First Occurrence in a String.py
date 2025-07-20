bc1e482c1162d920e416bc0c7f1551509380aeec
Zekai Zhao <zzhao775@uwo.ca>
2025-07-20 12:49:44 -0400

    Update 28. Find the Index of the First Occurrence in a String.py

--- LeetCode/28. Find the Index of the First Occurrence in a String.py
+++ LeetCode/28. Find the Index of the First Occurrence in a String.py
@@ -1,76 +1,48 @@
-# Cheat
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        return haystack.find(needle)
-
-# O(nm)
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        # return haystack.find(needle)
-
-        len_haystack = len(haystack)
-        len_needle = len(needle)
-
-        i = 0
-        j = 1
-        flag = 0
-
-        if len_needle > len_haystack:
-            return -1
-
-        while i < len_haystack:
-            j = 0
-            if haystack[i] == needle[0]:
-                flag = 1
-                while j < len_needle:
-                    if i + j >= len_haystack:
-                        return -1
-                    if haystack[i + j] == needle[j]:
-                        j += 1
-                    else:
-                        flag = 0
-                        break
-                if flag == 1:
-                    return i
-            i += 1
-
-        return -1
-
-
-# O(nm)
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        # return haystack.find(needle)
-
-        len_haystack = len(haystack)
-        len_needle = len(needle)
-
-        i = 0
-
-        if len_needle > len_haystack:
-            return -1
-
-        while i < len_haystack:
-            if i + len_needle > len_haystack:
-                return -1
-            elif haystack[i:i + len_needle] == needle:
-                return i
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) == 1 and len(needle) == 1:
+            if haystack == needle:
+                return 0
             else:
+                return -1
+
+        # Brute Force O(m * n)
+
+        # KMP O(m + n)
+        # Longest Proper Prefix which is also Suffix
+        # LPS
+        # "sadbutsad"
+        # 0,0,0,0,0,0,1,2,3
+        lps = [0] * len(needle)
+        len_ = 0
+        i = 1
+        while i < len(needle):
+            if needle[i] == needle[len_]:
+                len_ += 1
+                lps[i] = len_
                 i += 1
+            else:
+                if len_ == 0:
+                    i += 1
+                else:
+                    # same logic as below
+                    # image the prefix as needle here
+                    len_ = lps[len_ - 1]
+
+        print(lps)
+
+        # matching
+        i, j = 0, 0
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+                if j == len(needle):
+                    return i - j
+            else:
+                if j != 0:
+                    j = lps[j - 1]
+                elif j == 0:
+                    i += 1
 
         return -1
