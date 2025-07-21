e1b77430da74a8a47a8462e6f97c0c0ce70d7f32
Zekai Zhao <zzhao775@uwo.ca>
2025-07-21 15:18:09 -0400

    Update 392. Is Subsequence.py

--- LeetCode/392. Is Subsequence.py
+++ LeetCode/392. Is Subsequence.py
@@ -1,14 +1,26 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) > len(t):
-            return False
-        index_s = 0
-        for i in range(len(t)):
-            if index_s < len(s):
-                if t[i] == s[index_s]:
-                    index_s += 1
-            else:
-                return True
-        if index_s == len(s):
+        if len(s) == 0:
             return True
+        i = 0
+        for c in t:
+            if c == s[i]:
+                i += 1
+                if i == len(s):
+                    return True
         return False
+
+# class Solution:
+#     def isSubsequence(self, s: str, t: str) -> bool:
+#         if len(s) > len(t):
+#             return False
+#         index_s = 0
+#         for i in range(len(t)):
+#             if index_s < len(s):
+#                 if t[i] == s[index_s]:
+#                     index_s += 1
+#             else:
+#                 return True
+#         if index_s == len(s):
+#             return True
+#         return False  
