51958443ff69c00acba6eeec90e036bbedd9c5a5
Zekai Zhao <zzhao775@uwo.ca>
2025-07-24 17:58:06 -0400

    Update 54. Spiral Matrix.py

--- LeetCode/54. Spiral Matrix.py
+++ LeetCode/54. Spiral Matrix.py
@@ -1,61 +1,47 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # edge case
         result = []
-
-        # nums of rows
         m = len(matrix)
-        # nums of columns
         n = len(matrix[0])
-
-        num_of_elements = m * n
-
-        m_current = 0
-        n_current = 0
-
-        m_border_up = 1
-        m_border_down = m - 1
-
-        n_border_left = 0
-        n_border_right = n - 1
-
-        direction = 'right'
-
-        while len(result) != num_of_elements - 1:
-            if direction == 'right':
-                if n_current < n_border_right:
-                    result.append(matrix[m_current][n_current])
-                    n_current += 1
-                elif n_current == n_border_right:
-                    n_border_right -= 1
-                    direction = "down"
-
-            elif direction == "down":
-                if m_current < m_border_down:
-                    result.append(matrix[m_current][n_current])
-                    m_current += 1
-                elif m_current == m_border_down:
-                    m_border_down -= 1
-                    direction = "left"
-
-            elif direction == "left":
-                if n_current > n_border_left:
-                    result.append(matrix[m_current][n_current])
-                    n_current -= 1
-                elif n_current == n_border_left:
-                    n_border_left += 1
-                    direction = "up"
-
-            elif direction == "up":
-                if m_current > m_border_up:
-                    result.append(matrix[m_current][n_current])
-                    m_current -= 1
-                elif m_current == m_border_up:
-                    m_border_up += 1
-                    direction = "right"
-
-        result.append(matrix[m_current][n_current])
+        if m == 1:
+            return matrix[0]
+        if n == 1:
+            for l in matrix:
+                result.append(l[0])
+            return result
+        max_right = n - 1
+        max_left = 0
+        max_top = 1
+        max_bottom = m - 1
+        # 1, right, 2 down, 3 left, 4 up
+        state = 1
+        i, j = 0, 0
+        count = 0
+        while count < m * n:
+            result.append(matrix[i][j])
+            if state == 1:
+                j += 1
+                if j == max_right:
+                    max_right -= 1
+                    state = 2
+            elif state == 2:
+                i += 1
+                if i == max_bottom:
+                    max_bottom -= 1
+                    state = 3
+            elif state == 3:
+                j -= 1
+                if j == max_left:
+                    max_left += 1
+                    state = 4
+            elif state == 4:
+                i -= 1
+                if i == max_top:
+                    max_top += 1
+                    state = 1
+            count += 1
         return result
