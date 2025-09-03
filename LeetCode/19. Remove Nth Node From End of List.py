d5be631c01be267f18bfb04cd06abc622e6e19bc
Zekai Zhao <zzhao775@uwo.ca>
2025-09-03 18:27:55 -0400

    Update 19. Remove Nth Node From End of List.py

--- LeetCode/19. Remove Nth Node From End of List.py
+++ LeetCode/19. Remove Nth Node From End of List.py
@@ -6,30 +6,61 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # O(2n)
-        # Count
-        len = 0
-        head_temp = head
-        while head_temp:
-            len += 1
-            head_temp = head_temp.next
-        print(len)
-        # Find and delete
-        head_temp = head
-        dummy = ListNode(-1)
-        dummy.next = head
-        node_last = dummy
-        i = 0
+        list_node = []
+        node = head
+        while node:
+            list_node.append(node)
+            node = node.next
 
-        while head_temp:
-            if len - n - i == 0:
-                node_last.next = head_temp.next
-                break
-            else:
-                node_last = head_temp
-                head_temp = head_temp.next
-                i += 1
+        # edge case
+        if len(list_node) == 0 or len(list_node) == 1:
+            return None
+        # remove head
+        if n == len(list_node):
+            return head.next
+        # remove tail
+        if n == 1:
+            list_node[-2].next = None
+            return head
 
-        return dummy.next
+        # other
+        remove_node = list_node[len(list_node) - n]
+        node_front = list_node[len(list_node) - n - 1]
+        node_front.next = remove_node.next
+        remove_node.next = None
+
+        return head
+
+
+
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         # O(2n)
+#         # Count
+#         len = 0
+#         head_temp = head
+#         while head_temp:
+#             len += 1
+#             head_temp = head_temp.next
+#         print(len)
+#         # Find and delete
+#         head_temp = head
+#         dummy = ListNode(-1)
+#         dummy.next = head
+#         node_last = dummy
+#         i = 0
+#
+#         while head_temp:
+#             if len - n - i == 0:
+#                 node_last.next = head_temp.next
+#                 break
+#             else:
+#                 node_last = head_temp
+#                 head_temp = head_temp.next
+#                 i += 1
+#
+#         return dummy.next
