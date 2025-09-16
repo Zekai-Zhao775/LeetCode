f1691e87d90c455f21efea9569c21cc5e501bac7
Zekai Zhao <zzhao775@uwo.ca>
2025-09-16 13:11:54 -0400

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- LeetCode/236. Lowest Common Ancestor of a Binary Tree.py
+++ LeetCode/236. Lowest Common Ancestor of a Binary Tree.py
@@ -5,6 +5,42 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # add a pointer to parent node?
+        # then O(n) sacn, O(logn) find LCA, O(n) space
+        # use a {} to store this
+        # a bit too complicate
+
+        # dfs 2 times
+        # record the path
+        # return min path[-1]
+        # O(2n)
+
+        def dfs(node, target, path):
+            if not node:
+                return False
+            path.append(node)
+            if node == target:
+                return True
+            if dfs(node.left, target, path) or dfs(node.right, target, path):
+                return True
+            path.pop()
+            return False
+
+        path_p, path_q = [], []
+        dfs(root, p, path_p)
+        dfs(root, q, path_q)
+
+        LCA = None
+        for i in range(min(len(path_p), len(path_q))):
+            if path_p[i] == path_q[i]:
+                LCA = path_p[i]
+            else:
+                break
+
+        return LCA
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def allAncestor(root: 'TreeNode', target: 'TreeNode') -> list['TreeNode']:
