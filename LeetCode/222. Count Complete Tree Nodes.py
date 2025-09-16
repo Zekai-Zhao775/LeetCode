2ad622e24db7165ca5e6e12ff77fbf2e6451a1d1
Zekai Zhao <zzhao775@uwo.ca>
2025-09-16 12:49:43 -0400

    Create 222. Count Complete Tree Nodes.py

---
+++ LeetCode/222. Count Complete Tree Nodes.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        self.count = 0
+
+        def dfs(node):
+            if not node:
+                return
+            self.count += 1
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return self.count
