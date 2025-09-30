294df98adb30b6cd165ff46feaf91f97d2ec8a0d
Zekai Zhao <zzhao775@uwo.ca>
2025-09-30 09:06:34 -0400

    Update 67. Add Binary.py

--- LeetCode/67. Add Binary.py
+++ LeetCode/67. Add Binary.py
@@ -1,3 +1,7 @@
 class Solution:
+    # int("101", 2) → interpret "101" as binary → decimal 5
+    # int("77", 8) → interpret "77" as octal → decimal 63
+    # int("1F", 16) → interpret "1F" as hexadecimal → decimal 31
+    # int("123", 10) → interpret "123" as decimal → decimal 123
     def addBinary(self, a: str, b: str) -> str:
         return bin(int(a, 2) + int(b, 2))[2:]
