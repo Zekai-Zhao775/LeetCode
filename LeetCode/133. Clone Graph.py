a9d3cb317553c3537f44cfcec767e20281c88357
Zekai Zhao <zzhao775@uwo.ca>
2025-10-25 10:35:44 -0400

    Update 133. Clone Graph.py

--- LeetCode/133. Clone Graph.py
+++ LeetCode/133. Clone Graph.py
@@ -4,7 +4,6 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
@@ -17,26 +16,47 @@ class Solution:
         node_map = {}
         def dfs(node):
             if node in node_map:
-                return
+                return node_map[node]
             else:
                 newNode = Node(node.val)
                 node_map[node] = newNode
                 for n in node.neighbors:
-                    dfs(n)
-                return
-        dfs(node)
+                    newNode.neighbors.append(dfs(n))
+                return newNode
+        return dfs(node)
 
-        visited = set()
-        def dfs_connect(node):
-            if node in visited:
-                return
-            else:
-                visited.add(node)
-                for n in node.neighbors:
-                    node_map[node].neighbors.append(node_map[n])
-                for n in node.neighbors:
-                    dfs_connect(n)
-                return
-        dfs_connect(node)
+# from typing import Optional
+# class Solution:
+#     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+#         # use to dict to map node to newNode
+#         # this will function as a visited for first iteration too
+#         # (if key in dict, then visited)
+#         # iterate once to create all newNode (without neighbors)
+#         if not node:
+#             return None
+#         node_map = {}
+#         def dfs(node):
+#             if node in node_map:
+#                 return
+#             else:
+#                 newNode = Node(node.val)
+#                 node_map[node] = newNode
+#                 for n in node.neighbors:
+#                     dfs(n)
+#                 return
+#         dfs(node)
 
-        return node_map[node]
+#         visited = set()
+#         def dfs_connect(node):
+#             if node in visited:
+#                 return
+#             else:
+#                 visited.add(node)
+#                 for n in node.neighbors:
+#                     node_map[node].neighbors.append(node_map[n])
+#                 for n in node.neighbors:
+#                     dfs_connect(n)
+#                 return
+#         dfs_connect(node)
+
+#         return node_map[node]
