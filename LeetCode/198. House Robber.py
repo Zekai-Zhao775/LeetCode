154722cd276b0746e85d9bb978cc6bc6661ff032
Zekai Zhao <zzhao775@uwo.ca>
2025-11-09 12:38:41 -0500

    Update 198. House Robber.py

--- LeetCode/198. House Robber.py
+++ LeetCode/198. House Robber.py
@@ -3,6 +3,22 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # each stop rob and n+2, or n+1
+        dp = [-1] * len(nums)
+        def dp_help(i):
+            if i >= len(nums):
+                return 0
+            if dp[i] != -1:
+                return dp[i]
+            else:
+                dp[i] = max(nums[i] + dp_help(i + 2), dp_help(i + 1))
+                return dp[i]
+        dp_help(0)
+
+        return dp[0]
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
         dp = [0] * 400
